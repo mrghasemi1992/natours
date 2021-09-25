f90909cc21b0f5b9e27e0de1298b992030ca578b
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2021-09-26 01:20:47 +0330

    [ref] route and controllers separated from each other. * server.js added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const fs = require('fs');
 const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 // Middleware
 const app = express();
@@ -9,70 +11,8 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
+// Routes Middleware
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-// Route Handlers
-const getAllTours = (req, res) => {
-  res.json({ status: 'success', results: tours.length, data: { tours } });
-};
-
-const getTour = (req, res) => {
-  const id = Number(req.params.id);
-
-  const tour = tours.find((tour) => tour.id === id);
-
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID!' });
-  }
-
-  res.json({
-    status: 'success',
-    data: { tour },
-  });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({ status: 'success', data: { tour: newTour } });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  if (Number(req.params.id) > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID!' });
-  }
-
-  res.json({ status: 'success', data: { tour: '<Updated tour here...>' } });
-};
-
-const deleteTour = (req, res) => {
-  if (Number(req.params.id) > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID!' });
-  }
-
-  res.status(204).json({ status: 'success', data: null });
-};
-
-// Routes
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-
-app
-  .route('/api/v1/tours/:id')
-  .patch(updateTour)
-  .delete(deleteTour)
-  .get(getTour);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
