1069d4d139bb307d946f42b69c579ff0ea2ce56f
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2021-10-05 20:41:10 +0330

    [feat] MVC architecture was implemented * App was connected to MongoDB Atlas. * ESLint configurations were done. * Environment variables were added.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,54 +1,29 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
 exports.getAllTours = (req, res) => {
-  res.json({ status: 'success', results: tours.length, data: { tours } });
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This route is not yet defined!' });
 };
 
 exports.getTour = (req, res) => {
-  const id = Number(req.params.id);
-
-  const tour = tours.find((tour) => tour.id === id);
-
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID!' });
-  }
-
-  res.json({
-    status: 'success',
-    data: { tour },
-  });
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This route is not yet defined!' });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({ status: 'success', data: { tour: newTour } });
-    }
-  );
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This route is not yet defined!' });
 };
 
 exports.updateTour = (req, res) => {
-  if (Number(req.params.id) > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID!' });
-  }
-
-  res.json({ status: 'success', data: { tour: '<Updated tour here...>' } });
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This route is not yet defined!' });
 };
 
 exports.deleteTour = (req, res) => {
-  if (Number(req.params.id) > tours.length) {
-    return res.status(404).json({ status: 'fail', message: 'Invalid ID!' });
-  }
-
-  res.status(204).json({ status: 'success', data: null });
+  res
+    .status(500)
+    .json({ status: 'error', message: 'This route is not yet defined!' });
 };
--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+/* eslint-disable no-console */
+
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful!');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
