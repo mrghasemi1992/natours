b237e99ea8f2eaae3e952857fc19a330ce7e6873
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2021-10-15 00:29:53 +0330

    [ref] APIFeatures class

---
+++ utils/APIFeatures.js
@@ -0,0 +1,54 @@
+class APIFeatures {
+  constructor(query, queryStr) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryStr };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
+
+    return this;
+  }
+
+  sort() {
+    if (this.queryStr.sort) {
+      const sortBy = this.queryStr.sort.split(',').join(' ');
+
+      this.query = this.query.sort(sortBy);
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+
+    return this;
+  }
+
+  limitedFields() {
+    if (this.queryStr.fields) {
+      const fields = this.queryStr.fields.split(',').join(' ');
+      this.query = this.query.select(fields);
+    } else {
+      this.query = this.query.select('-__v');
+    }
+
+    return this;
+  }
+
+  paginate() {
+    const page = Number(this.queryStr.page) || 1;
+    const limit = Number(this.queryStr.limit) || 100;
+    const skip = limit * (page - 1);
+
+    this.query = this.query.skip(skip).limit(limit);
+
+    return this;
+  }
+}
+
+module.exports = APIFeatures;
