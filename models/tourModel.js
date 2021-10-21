fc9726470d668fb16347b3b812d45f63be6d8568
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2021-10-21 23:54:55 +0330

    [feat] I added some validators to the tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,14 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name!'],
       unique: true,
       trim: true,
+      maxlength: [
+        40,
+        'A tour name must have less or equal than 40 characters.',
+      ],
+      minlength: [
+        10,
+        'A tour name must have more or equal than 10 characters.',
+      ],
     },
     slugify: String,
     duration: {
@@ -21,10 +29,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty!'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either easy, medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
@@ -34,7 +48,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price!'],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only points to the current doc on new document creation
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below the regular price!',
+      },
+    },
     summary: {
       type: String,
       required: [true, 'A tour must have a summary!'],
@@ -70,7 +93,7 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
-// Document middleware
+// Document middleware: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slugify = slugify(this.name, { lower: true });
   next();
