42fc4047a739bb6340bb8c411826a8975104a44b
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2021-11-01 18:50:28 +0330

    [wip] Error handling * appError class * erroController

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 // Middleware
 const app = express();
@@ -16,10 +18,15 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
+  const err = new AppError(
+    `Can't find ${req.originalUrl} on this server!`,
+    404
+  );
+
+  next(err);
 });
+
+// Global Error Handling Middleware
+app.use(globalErrorHandler);
 
 module.exports = app;
