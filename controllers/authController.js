560070b7c97d62e89ad88ef09b153ae8d304850d
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2021-11-19 08:21:18 +0330

    [bug] A bug in protect controller was resolved/

--- controllers/authController.js
+++ controllers/authController.js
@@ -83,7 +83,7 @@ exports.protect = catchAsync(async (req, res, next) => {
   }
 
   // 4) Check if user changed password after the token was issued
-  if (currentUser.passwordChangedAfter) {
+  if (currentUser.passwordChangedAt) {
     return next(
       new AppError('User recently changed password! Please log in again', 401)
     );
