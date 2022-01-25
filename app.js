cf0964270d63fd086cc84e2fe9b52ccb53701c4a
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2022-01-25 20:26:59 +0330

    [feat] rate limiter

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
-// Middleware
 const app = express();
 
+// Global middlewares
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
 
 app.use(express.json());
 
-// Routes Middleware
+// Routes middleware
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
