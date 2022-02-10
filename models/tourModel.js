c9c88395097d1a2f4e0053a605f679e27834a334
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2022-02-10 16:13:24 +0330

    [feat] Virtual populate reviews for a tour get service

--- models/tourModel.js
+++ models/tourModel.js
@@ -112,6 +112,13 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
+// Virtual populate
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id',
+});
+
 // Document middleware: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slugify = slugify(this.name, { lower: true });
