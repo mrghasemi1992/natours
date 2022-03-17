7d426c9ef9e366c3b01eab537696441bdfe70b37
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2022-03-17 12:46:40 +0330

    [wip] hanlder factory

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,6 +1,18 @@
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
     const doc = await Model.findByIdAndDelete(req.params.id);
